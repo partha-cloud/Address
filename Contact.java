53bf5812ec51f42998d9e80c079836b1f8285b18
partha-cloud <partha11dey@gmail.com>
2021-03-16 22:47:41 +0530

    ADDED UC2

--- Contact.java
+++ Contact.java
@@ -1,32 +1,135 @@
 package Address;
-
+import java.util.Scanner;
 public class Contact {
-	String FIRST_NAME;
-	String LAST_NAME;
-	String ADDRESS;
-	String CITY;
-	String STATE;
-	String POSTAL_CODE;
-	String COUNTRY;
-	String CONTACT_NUMBER;
-	String EMAIL;
-	Contact(String fn,String ln,String add,String city,String state,String zip,String country,String mob,String mail)
+	Scanner sc=new Scanner(System.in);
+	static Contact[] contactList=new Contact[15];
+	public Contact() {
+		// TODO Auto-generated constructor stub
+	}
+	public Contact(String fn, String ln, String add, String city, String state, String zip, String country, String mob,
+			String mail) {
+		// TODO Auto-generated constructor stub
+	}
+	int add(int increment)
 	{
-		this.FIRST_NAME=fn;
-		this.LAST_NAME=ln;
-		this.ADDRESS=add;
-		this.CITY=city;
-		this.STATE=state;
-		this.POSTAL_CODE=zip;
-		this.COUNTRY=country;
-		this.CONTACT_NUMBER=mob;
-		this.EMAIL=mail;
+		System.out.println("Enter First Name:");
+		String fn=sc.next();
+		System.out.println("Enter last name:");
+		String ln=sc.next();
+		System.out.println("Enter Address:");
+		String add=sc.next();
+		System.out.println("Enter City:");
+		String city=sc.next();
+		System.out.println("Enter State:");
+		String state=sc.next();
+		System.out.println("Enter postal_code:");
+		String zip=sc.next();
+		System.out.println("Enter Country_name");
+		String country=sc.next();
+		System.out.println("Enter contact number:");
+		String mob=sc.next();
+		System.out.println("Enter email :");
+		String mail=sc.next();
+		contactList[increment++]=new Contact(fn, ln, add, city, state,zip,country,mob,mail);
+		return increment;
+	 }
+	void edit(int increment)
+	{
+		int flag;
+		for(flag=0;flag<10;flag++)
+		{
+		if(flag==increment)
+		{
+			System.out.println("Enter First Name:");
+			String fn=sc.next();
+      	System.out.println("Enter last name:");
+      	String ln=sc.next();
+      	System.out.println("Enter Address:");
+      	String add=sc.next();
+      	System.out.println("Enter City:");
+      	String city=sc.next();
+      	System.out.println("Enter State:");
+      	String state=sc.next();
+      	System.out.println("Enter postal_code:");
+      	String zip=sc.next();
+      	System.out.println("Enter Country:");
+      	String country=sc.next();
+      	System.out.println("Enter contact number:");
+      	String mob=sc.next();
+      	System.out.println("Enter email :");
+      	String mail=sc.next();
+      	contactList[increment++]=new  Contact(fn, ln, add, city, state,zip,country,mob,mail);
+			break;
+		}
+		}
+		System.out.println("Your changes replicated to the Book manager successfully....");
+		
+	}
+	void remove(int increment)
+	{
+		int flag;
+		for(flag=0;flag<15;flag++)
+			{
+				if(flag==increment)
+				{
+					contactList[increment]=null;
+					break;
+				}
+			}
+		System.out.println("!!Deleted!!");
 	}
 
-	public String toString() {
-		return String.format(FIRST_NAME+" "+LAST_NAME+" "+ADDRESS+" "+CITY+" "+STATE+" "+POSTAL_CODE+" "
-									 +COUNTRY+" "+CONTACT_NUMBER+" "+EMAIL);
+	void display()
+	{
+		for(int increment=0;increment<15;increment++)
+		{
+			System.out.println(increment+" "+contactList[increment]);
+		}
 	}
-
-}
-
+public static void main(String args[]){
+	System.out.println("---------------WELCOME TO THE ADDRESS_BOOK_MANAGEMENT SYSTEM-------------"); 
+	Scanner sc=new Scanner(System.in);
+	Contact ab=new Contact();
+	int increment=0;
+	System.out.println("Enter valid option to perform Book-Manager_Application(1-->ENTER,2-->EXIT):");
+	int option=sc.nextInt();
+		while(option !=2) {
+			System.out.println("select the operations(1,2,3) you want to perform 1)Add 2)Edit 3)Remove 4)display \n");
+			int choose =sc.nextInt();
+			switch(choose) 
+			{
+				case 1:
+					System.out.println("Adding credentials to the Application");
+					increment=ab.add(increment);
+					break;
+				case 2:
+					System.out.println("welcome to the editing mode ..!");
+					System.out.println("Enter the row number(record) you want to edit");
+					int record_edit=sc.nextInt();
+					ab.edit(record_edit);
+					ab.display();
+					break;
+				case 3:
+					System.out.println("currently you are in Delete-mode ,once you delete it's not revert back \n");
+					System.out.println("Enter record number to delete :");
+					int record_del=sc.nextInt();
+					ab.remove(record_del);
+					break;
+				case 4:
+					System.out.println("-------These are the Records avalibale in your Application------- \n");
+					ab.display();
+					break;
+				default:
+					System.out.println("select valid operation[1/2/3/4]");
+			}
+		System.out.println("Enter choice(1-Enter/2-Exit):");
+		int option1=sc.nextInt();
+		if (option1==2)
+			{
+				System.out.println("OOPS!! you are choosing an Exit mode");
+				break;
+			}
+			}
+		System.out.println("THANKS FOR USING BOOK MANAGER-APPLICATION");
+	}
+}		
